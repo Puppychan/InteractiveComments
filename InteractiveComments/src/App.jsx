e20f638ef8a52e7f8118c02b75350b66f2b1736a
Matsuri <nhungmaitran1412@gmail.com>
2022-10-07 14:16:48 +0700

    add local storage

--- InteractiveComments/src/App.jsx
+++ InteractiveComments/src/App.jsx
@@ -1,3 +1,4 @@
+//https://stackoverflow.com/questions/60618844/react-hooks-useeffect-is-called-twice-even-if-an-empty-array-is-used-as-an-ar
 import { useState, useEffect } from 'react'
 import GlobalStyled from './Style/GlobalStyled'
 import {CommentsContext} from "./Helpers/Contexts"
@@ -7,9 +8,28 @@ import Comments from "./Components/Comments"
 
 function App() {
 
-  const [comments, setComments] = useState(JsonData.comments)
+  const [comments, setComments] = useState([])
   const [currentClickReplies, setCurrentClickReplies] = useState(new Set())
   const currentUser = JsonData.currentUser
+
+  useEffect(() => {
+    const localStoreItems = localStorage.getItem('comments')
+    if (localStoreItems.length != 2) {
+      const items = JSON.parse(localStoreItems)
+      console.log("not undefine-", items);
+      setComments(items)
+    }
+    else {
+      console.log('undefine', localStoreItems);
+      localStorage.setItem('comments', JSON.stringify(JsonData.comments))
+      setComments(JsonData.comments)
+    }
+    
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('comments', JSON.stringify(comments))
+  }, [comments])
 
   return (
     <CommentsContext.Provider value={{comments, setComments, currentUser, currentClickReplies, setCurrentClickReplies}}>
