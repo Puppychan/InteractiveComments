7d7950c6647fe5659e265289ac5f798628dd6229
Matsuri <nhungmaitran1412@gmail.com>
2022-10-08 22:38:49 +0700

    fix deploy run build

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import * as constStyle from "../../Style/ConstantStyled"
+import avatar from "../../assets/avatars/image-amyrobson.png"
 import { AvatarStyle } from './Avatar.style'
 
 const Avatar = ({username, gridArea}) => {
   return (
     <AvatarStyle 
       gridArea={gridArea}
-      src={constStyle.getImgUrl("avatar", username)} 
+      // src={constStyle.getImgUrl("avatar", username)} 
+      src={avatar}
       alt={`${username}'s avatar`} 
     />
   )
